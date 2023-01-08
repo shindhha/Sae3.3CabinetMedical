94923a9e1ab2a9eb00f5e3d5f2580be650e80759
shindhha <99216728+shindhha@users.noreply.github.com>
2023-01-08 21:04:08 +0100

    Remaniement de la base de données

--- interne.sql
+++ interne.sql
@@ -1,14 +1,16 @@
 DROP TABLE IF EXISTS ListeVisites;
+DROP TABLE IF EXISTS PatientsMedecins;
 DROP TABLE IF EXISTS Patients;
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS Medecins;
 DROP TABLE IF EXISTS Visites;
 DROP TABLE IF EXISTS Cabinet;
 DROP TABLE IF EXISTS Ordonnances;
-DROP TABLE IF EXISTS users;
+
 
 CREATE TABLE Patients (
 	numSecu CHAR(13) PRIMARY KEY,
-	LieuNaissance INT(6),
+	LieuNaissance VARCHAR(100),
 	nom VARCHAR(25),
 	prenom VARCHAR(25),
 	dateNaissance DATE,
@@ -16,7 +18,9 @@ CREATE TABLE Patients (
 	codePostal INT(5),
 	medecinRef INT(11),
 	numTel INT(9),
-	email VARCHAR(50)
+	email VARCHAR(50),
+	notes TEXT,
+	sexe bool
 );
 
 CREATE TABLE Medecins (
@@ -35,18 +39,23 @@ CREATE TABLE Medecins (
 	lieuAct VARCHAR(100)	
 );
 CREATE TABLE Visites (
-	idVisite INT(6),
+	idVisite INT(6) AUTO_INCREMENT,
 	motifVisite TEXT,
 	dateVisite DATE,
 	note TEXT,
 	PRIMARY KEY (idVisite)
 );
 
-CREATE TABLE ListeVisites (
+CREATE TABLE PatientsMedecins (
 	numSecu CHAR(13),
 	numRPPS CHAR(11),
+	PRIMARY KEY (numRPPS,numSecu)
+);
+
+CREATE TABLE ListeVisites (
+	numSecu CHAR(13),
 	idVisite INT(6),
-	PRIMARY KEY (numRPPS,numSecu,idVisite)
+	PRIMARY KEY (idVisite,numSecu)
 );
 
 CREATE TABLE Cabinet (
@@ -57,18 +66,20 @@ CREATE TABLE Cabinet (
 CREATE TABLE Ordonnances (
 	idOrdonnance INT(6),
 	codeCIS INT(8),
+	instruction TEXT,
 	PRIMARY KEY (idOrdonnance,codeCIS)
 );
 CREATE TABLE users (
-	id INT(6),
+	id INT(6) AUTO_INCREMENT PRIMARY KEY,
 	login VARCHAR(11),
 	`password` CHAR(32)
 
 );
 ALTER TABLE Ordonnances ADD CONSTRAINT FK_Ordonnances_Medicaments FOREIGN KEY (codeCIS) REFERENCES CIS_BDPM(codeCIS);
 ALTER TABLE Patients ADD CONSTRAINT CK_Email_Patients CHECK (email LIKE '%@%.%');
+ALTER TABLE PatientsMedecins ADD CONSTRAINT FK_PatientsMedecins_Patients FOREIGN KEY (numSecu) REFERENCES Patients(numSecu);
+ALTER TABLE PatientsMedecins ADD CONSTRAINT FK_PatientsMedecins_Medecins FOREIGN KEY (numRPPS) REFERENCES Medecins(numRPPS);
+ALTER TABLE ListeVisites ADD CONSTRAINT FK_ListeVisites_Visites FOREIGN KEY (idVisite) REFERENCES Visites(idVisite);
 ALTER TABLE ListeVisites ADD CONSTRAINT FK_ListeVisites_Patients FOREIGN KEY (numSecu) REFERENCES Patients(numSecu);
-ALTER TABLE ListeVisites ADD CONSTRAINT FK_ListeVisites_Medecins FOREIGN KEY (numRPPS) REFERENCES Medecins(numRPPS);
-ALTER TABLE Visites ADD CONSTRAINT FK_Visites_Ordonnances FOREIGN KEY (idVisite) REFERENCES Ordonnances(idOrdonnance);
+ALTER TABLE Ordonnances ADD CONSTRAINT FK_Visites_Ordonnances FOREIGN KEY (idOrdonnance) REFERENCES Visites(idVisite);
 ALTER TABLE Medecins ADD CONSTRAINT CK_Email_Medecins CHECK (email LIKE '%@%.%');
-ALTER TABLE users ADD CONSTRAINT FK_users_Medecins FOREIGN KEY (login) REFERENCES Medecins(numRPPS);
